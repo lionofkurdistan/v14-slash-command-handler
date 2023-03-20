aecd88471dd705fdf14821a08a66edc232b3a42b
Dev Lion Of Kurdistan <76536335+lionofkurdistan@users.noreply.github.com>
2023-03-20 23:58:55 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  prefix: "!",
-  owner: "598718975735758848",
-  token: "MTA4MTg2MTAyMjkyNDY4NTM4Mg.GbtV7f.FD3J4yosEkbb3d_hpnhyDHBcD_fJGmsVOzyvZs",
+  prefix: "PREFIX",
+  owner: "ID OWNER",
+  token: "TOKEN",
 }
