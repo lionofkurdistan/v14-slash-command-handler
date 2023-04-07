4b34444e1bf322a959026bbcde754c54421bfc15
HAJY01 <108593987+HAJY01@users.noreply.github.com>
2023-04-07 16:57:46 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  prefix: "PREFIX",
-  owner: "ID OWNER",
-  token: "TOKEN",
+  prefix: "✹",
+  owner: "781455841500332032",
+  token: "MTA4NjMyMzQ3NDI2MTM2MDY3MA.GkJRNn._LgARhMxzhlBvcZzCGFYs31D0TBL2pZSDHeKDE",
 }
